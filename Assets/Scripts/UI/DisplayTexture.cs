a402df38650e665ee9957dcabc9316f199077e1a
Daniil1984195 <ryj5244@autuni.ac.nz>
2024-09-12 13:58:21 +1200

    Configure UI so that it works

--- Assets/Scripts/UI/DisplayTexture.cs
+++ Assets/Scripts/UI/DisplayTexture.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DisplayRenderTexture : MonoBehaviour
+public class DisplayTexture : MonoBehaviour
 {
     public RawImage rawImage;
     public string previewSceneName = "Preview Scene";
