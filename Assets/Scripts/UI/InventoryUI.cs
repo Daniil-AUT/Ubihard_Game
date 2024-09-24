1404e58cd7b6624c4daf5d1182ee3bddb9ed3967
Daniil-AUT <ryj5244@autuni.ac.nz>
2024-09-24 19:27:57 +1200

    Add more aesthetic UI item elements, fix the ui script

--- Assets/Scripts/UI/InventoryUI.cs
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -1,43 +1,33 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    public Inventory inventory;
-    public Image[] itemSlots; // Assign the UI image slots in the Inspector
+    public Image skeletonKeyIcon;
+    public Image mpPotionIcon;
+    public Image hpPotionIcon;
 
-    void Start()
+    [SerializeField] private ItemSO skeletonKeyItem;
+    [SerializeField] private ItemSO mpPotionItem;
+    [SerializeField] private ItemSO hpPotionItem;
+
+    private void Start()
     {
-        inventory.onItemChangedCallback += UpdateUI;
-        UpdateUI(); // Initial call to update UI
+        // Initially hide all icons
+        skeletonKeyIcon.gameObject.SetActive(false);
+        mpPotionIcon.gameObject.SetActive(false);
+        hpPotionIcon.gameObject.SetActive(false);
     }
 
-    void UpdateUI()
+    private void Update()
     {
-        for (int i = 0; i < itemSlots.Length; i++)
-        {
-            if (i < inventory.items.Count)
-            {
-                itemSlots[i].sprite = inventory.items[i].icon; // Show item icon
-                itemSlots[i].gameObject.SetActive(true); // Show the slot
-            }
-            else
-            {
-                itemSlots[i].gameObject.SetActive(false); // Hide empty slots
-            }
-        }
+        UpdateItemVisibility();
     }
 
-    void Update()
+    private void UpdateItemVisibility()
     {
-        // Handle item activation using keys 1, 2, 3, 4
-        for (int i = 0; i < itemSlots.Length; i++)
-        {
-            if (Input.GetKeyDown((i + 1).ToString())) // Check for keys 1-4
-            {
-                inventory.ActivateItem(i);
-            }
-        }
+        skeletonKeyIcon.gameObject.SetActive(InventoryManager.Instance.HasItem(skeletonKeyItem));
+        mpPotionIcon.gameObject.SetActive(InventoryManager.Instance.HasItem(mpPotionItem));
+        hpPotionIcon.gameObject.SetActive(InventoryManager.Instance.HasItem(hpPotionItem));
     }
 }
